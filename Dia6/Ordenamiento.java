26e74a1c003238f5a9315a10aed09220e200f67a
DanielKano <markpolo137@gmail.com>
2023-05-26 12:07:07 -0500

    dia 6

---
+++ Dia6/Ordenamiento.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ordenamiento {
+    static Scanner leer ;
+    static int [] num1;
+    public static void main(String[] args) {
+        int cant;
+        System.out.println("ingresa la cantidad el numero de numeros a cargar");
+        cant = leer.nextInt();
+        num1 = new int[cant];
+        for (int i = 0; i < cant; i++){
+            System.out.println("ingresa el numero de la pocicion: "+(i+1));
+            num1[i]=leer.nextInt();
+        }
+        for (int j = 0; j < cant; j++){
+            for (int i = 0; i < cant-j; i++){
+                if(num1[i]>num1[i+1]){
+                    int aux;
+                    aux=num1[i];
+                    num1 [i]=num1 [i+1];
+                    num1 [i+1]=aux;
+                }
+            }
+        }
+        System.out.println("numero de menor a mayor");
+        for (int i = 0; i < num1.length; i++){
+            System.out.println(num1[i]);
+        }
+    }
+}
